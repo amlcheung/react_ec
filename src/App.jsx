979ca5f7f8f9fc97eeecc0b124c7fe30095d95ba
Ashley Cheung <ashleycheung@Ashleys-MacBook-Air.local>
2022-03-02 14:12:46 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div class="grid-container">
+            <div class="header template-color">
+            </div>
+            <div class="name-container">
+                <div class="my-name-format">
+                    <h1>Ashley's Website</h1>
+                </div>
+                <div class="box-container">
+                    <div class="box"></div>
+                </div>
+            </div>
+            <div class="left-side template-color">
+            </div>
+            <div class="main template-color">
+            </div>
+            <div class="right-side template-color">
+            </div>
+            <div class="footer template-color">
+            </div>
+        </div>
+
+  );
+}
+
+export default App;
